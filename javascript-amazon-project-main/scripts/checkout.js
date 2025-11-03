3188263a5e5c4b702fd9ae5b5a3c0a95e10b3419
Madhankannan02 <madhankannan29@gmail.com>
2025-11-03 09:14:10 +0530

    Final Commit

---
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -0,0 +1,63 @@
+import {renderOrderSummary} from './checkout/orderSummary.js';
+import {renderPaymentSummary} from './checkout/paymentSummary.js';
+//import '../data/cart-class.js';
+//import '../data/backend-practice.js';
+import { loadProducts, loadProductsFetch } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
+
+async function loadPage(){
+
+  try{
+    //throw 'eeror1';
+    await loadProductsFetch();
+
+    const value = await new Promise((resolve, reject)=>{
+      loadCart(()=>{
+       //reject('error');
+       resolve();
+      });
+    });
+  } catch (error){
+    console.log('Unexpected error. Please try again later');
+  }
+
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+
+loadPage();
+
+
+/*Promise.all([
+ loadProductsFetch(),
+  new Promise((resolve)=>{
+  loadCart(()=>{
+   resolve();
+  });
+}),
+
+]).then(()=>{
+ renderOrderSummary();
+ renderPaymentSummary();
+});*/
+
+/*
+new Promise((resolve)=>{
+ loadProducts(()=>{
+ resolve('value1');
+ });
+
+}).then((value)=>{
+ return new Promise((resolve)=>{
+  loadCart(()=>{
+   resolve();
+  });
+ });
+
+}).then(()=>{
+ renderOrderSummary();
+ renderPaymentSummary();
+});
+
+*/
---
+++ javascript-amazon-project-main/tests/utils/moneyTest.js
@@ -0,0 +1,15 @@
+import {formatCurrency} from '../../scripts/utils/money.js';
+
+describe('Test suite: formatCurrency', ()=>{
+ it ('converts cents into dollars',()=>{
+  expect(formatCurrency(2095)).toEqual('20.95');
+ });
+
+ it('works with 0',()=>{
+  expect(formatCurrency(0)).toEqual('0.00');
+ });
+
+ it('rounds upto the neares cents',()=>{
+  expect(formatCurrency(2000.4)).toEqual('20.00');
+ });
+});
